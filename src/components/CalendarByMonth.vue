cfd02d7a3a4823db1393ce4f39d073099389b36c
Axelle <guinaudeau.ion1516@gmail.com>
2023-09-12 16:08:43 +0200

    [MR-17] feat :start calendar by month

---
+++ src/components/CalendarByMonth.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col v-for="item in weekdays" :key="item">
+          {{ item }}
+        </v-col>
+      </v-row>
+      <v-row> </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalendarByMonth",
+  data() {
+    return {
+      today: null,
+      weekdays: [
+        "Lundi",
+        "Mardi",
+        "Mercredi",
+        "Jeudi",
+        "Vendredi",
+        "Samedi",
+        "Dimanche",
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,32 +1,165 @@
 <template>
-  <AppointmentComponent v-if="isVisible" @close-popin="closePopin"></AppointmentComponent>
+  <AppointmentComponent
+    v-if="isVisible"
+    @close-popin="closePopin"
+  ></AppointmentComponent>
   <div>
-    <v-btn @click="isVisible = !isVisible">AJOUT RDV</v-btn>
+    <v-row class="fill-height">
+      <v-col>
+        <v-sheet height="64">
+          <v-toolbar flat>
+            <v-btn
+              outlined
+              class="mr-4"
+              color="grey darken-2"
+              @click="setToday"
+            >
+              AUjourd'hui
+            </v-btn>
+            <v-btn fab text small color="grey darken-2" @click="prev">
+              <v-icon small> mdi-chevron-left </v-icon>
+            </v-btn>
+            <v-btn fab text small color="grey darken-2" @click="next">
+              <v-icon small> mdi-chevron-right </v-icon>
+            </v-btn>
+            <v-toolbar-title v-if="$refs.calendar">
+              {{ $refs.calendar.title }}
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn @click="isVisible = !isVisible">AJOUT RDV</v-btn>
+
+            <v-menu bottom right>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+                  <span>{{ typeToLabel[type] }}</span>
+                  <v-icon right> mdi-menu-down </v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="type = 'day'">
+                  <v-list-item-title>Day</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'week'">
+                  <v-list-item-title>Week</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = 'month'">
+                  <v-list-item-title>Month</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="type = '4day'">
+                  <v-list-item-title>4 days</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar>
+        </v-sheet>
+        <v-sheet height="600">
+          <CalendarByMonth></CalendarByMonth>
+          <!-- <v-calendar
+            ref="calendar"
+            v-model="focus"
+            color="primary"
+            :events="events"
+            :event-color="getEventColor"
+            :type="type"
+            @click:event="showEvent"
+            @click:more="viewDay"
+            @click:date="viewDay"
+            @change="updateRange"
+          ></v-calendar> -->
+          <v-menu
+            v-model="selectedOpen"
+            :close-on-content-click="false"
+            :activator="selectedElement"
+            offset-x
+          >
+            <v-card color="grey lighten-4" min-width="350px" flat>
+              <v-toolbar :color="selectedEvent.color" dark>
+                <v-btn icon>
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+                <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>mdi-heart</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </v-toolbar>
+              <v-card-text>
+                <span v-html="selectedEvent.details"></span>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="secondary" @click="selectedOpen = false">
+                  Cancel
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+        </v-sheet>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-
-import AppointmentComponent from '@/components/AppointmentComponent';
+import AppointmentComponent from "@/components/AppointmentComponent";
+import CalendarByMonth from "@/components/CalendarByMonth";
 export default {
-  name: 'CalendarView',
-  components: {AppointmentComponent},
+  name: "CalendarView",
+  components: { AppointmentComponent, CalendarByMonth },
   data() {
     return {
-      isVisible: false
-    }
+      isVisible: false,
+      today: new Date().toISOString().substr(0, 10), // date format 2019-09-01
+      focus: new Date().toISOString().substr(0, 10), // date format 2019-09-01
+      type: "month",
+      typeToLabel: {
+        month: "Month",
+        week: "Week",
+        day: "Day",
+        "4day": "4 Days",
+      },
+      name: null,
+      details: null,
+      start: null,
+      end: null,
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      events: [],
+      colors: [
+        "blue",
+        "indigo",
+        "deep-purple",
+        "cyan",
+        "green",
+        "orange",
+        "grey darken-1",
+      ],
+      names: [
+        "Meeting",
+        "Holiday",
+        "PTO",
+        "Travel",
+        "Event",
+        "Birthday",
+        "Conference",
+        "Party",
+      ],
+    };
   },
   methods: {
     closePopin() {
       this.isVisible = false;
     },
   },
-}
+};
 </script>
 
 <style scoped>
 button {
-  background-color: #262DB7;
+  background-color: #262db7;
   color: white;
 }
 </style>
