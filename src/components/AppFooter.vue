44d3b5ea9145b18c962af953a325dbc3706d76d1
Axelle <guinaudeau.ion1516@gmail.com>
2023-09-13 19:52:11 +0200

    [MR-17] style : fix footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,30 +1,29 @@
 <template>
-  <section style="
-    background-color: #000000;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 95px;
-  ">
+  <section
+    style="
+      background-color: #000000;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    "
+  >
     <p
       style="
-        color: #FFFFFF;
+        color: #ffffff;
         font-size: 14px;
         padding: 2em 8em;
         text-decoration: white underline;
       "
     >
-      Conditions générales d'utilisation<br/>
-      Contact<br/>
+      Conditions générales d'utilisation<br />
+      Contact<br />
       FAQ
     </p>
   </section>
 </template>
 <script>
-
 export default {
-  name: 'AppFooter',
-}
-
+  name: "AppFooter",
+};
 </script>
