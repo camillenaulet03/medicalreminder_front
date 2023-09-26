dffdd99e38f647e3ef278f5704b4747a74874e59
camillenaulet03 <45195515+camillenaulet03@users.noreply.github.com>
2023-09-26 10:01:41 +0200

    add toast success

--- src/views/ChangeRoleView.vue
+++ src/views/ChangeRoleView.vue
@@ -43,7 +43,7 @@ export default {
       if (this.email !== null && this.email !== "") {
         await authService.changeRole(userBody).then(async () => {
           //À mettre un route navigate
-          console.log("Rôle changé");
+          toast.success("Rôle modifié");
         }).catch(() => {
           toast.error("L'email est incorrect !")
         });
